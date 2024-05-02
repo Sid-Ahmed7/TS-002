cde0aed63cf3e75f5484522eb2ca2d023c959ab6
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-05-02 16:19:11 +0200

    Add TS-002

---
+++ build/challenge.js
@@ -0,0 +1,15 @@
+"use strict";
+const prettyPrintWilder = (users) => {
+    users.map((user) => {
+        console.log(`${user.name} is ${user.age} years old`);
+    });
+};
+const wilders = [];
+const user1 = { name: "Pierre", age: 23 };
+const user2 = { name: "Paul", age: new Date().getFullYear() - 1990 };
+const user3 = { name: "Jacques", age: 25 };
+wilders.push(user1);
+wilders.push(user2);
+wilders.push(user3);
+prettyPrintWilder(wilders);
+//# sourceMappingURL=challenge.js.map
